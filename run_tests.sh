e3eee00ac4854eccc65a16e25de8a7af577758fb
Rotem Braun <rotem.braun@gmail.com>
2025-04-09 02:11:10 +0300

    add tests to post-parser

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Convert line endings (do this once manually from Windows editor or with dos2unix/sed)
-sed -i 's/\r$//' run_tests.sh
-
 # Build the Docker image
 docker build -f Dockerfile_tests -t schedulify-test .
 
-# Run the container and execute tests inside the build directory
+# Run the built Docker container
 docker run -it --rm -v "$(pwd):/usr/src/app" -w /usr/src/app/tests/build schedulify-test
